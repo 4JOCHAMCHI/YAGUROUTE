ea91ce0fa49468345332a717462a6d2f780b309d
goeunpark123 <goeunpark21@gmail.com>
2024-07-20 10:53:37 +0900

    Init: Add backend project folder

---
+++ yagu-route/src/main/java/org/teamtuna/yaguroute/YaguRouteApplication.java
@@ -0,0 +1,9 @@
+package org.teamtuna.yaguroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaguRouteApplication {
+	public static void main(String[] args) { SpringApplication.run(YaguRouteApplication.class, args); }
+}
