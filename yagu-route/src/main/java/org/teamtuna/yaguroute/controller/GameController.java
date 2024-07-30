688d1d7f0d04b53291cf59dad98114d3ad399d98
jaehyeon-SMU <81796274+jaehyeon-SMU@users.noreply.github.com>
2024-07-30 12:39:11 +0900

    Refactor: Update GameController
    앤드포인트 Detail >> detail로 수정

--- yagu-route/src/main/java/org/teamtuna/yaguroute/controller/GameController.java
+++ yagu-route/src/main/java/org/teamtuna/yaguroute/controller/GameController.java
@@ -42,7 +42,7 @@ public class GameController {
         return gameService.getGamesByStadium(stadium);
     }
 
-    @GetMapping("Detail/{gameId}")
+    @GetMapping("detail/{gameId}")
     public GameDetailDTO getGameDetailById(@PathVariable int gameId) {
         return gameService.getGameDetailById(gameId);
     }
